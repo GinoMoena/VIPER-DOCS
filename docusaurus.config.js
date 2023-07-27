c31bfe25e227464cd3d67ea47ba2d18917c63b19
Gino Moena <gino.moena@gmail.com>
2023-07-27 19:00:18 -0400

    configupddted

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,17 +11,18 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://viper-docs.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Gino Moena', // Usually your GitHub org/user name.
+  projectName: 'VIPER-DOCS', // Usually your repo name.
+  deploymentBranch: 'main',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
